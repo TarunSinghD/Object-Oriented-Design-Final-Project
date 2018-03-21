25d0afe884b3407ec255bc71b7bab20cce6cf926
Vennela Gandluri <vennela@Mahadevs-MacBook-Pro.local>
2018-03-20 21:45:41 -0600

    Added user and role

--- LeaveManagement/src/User.java
+++ LeaveManagement/src/User.java
@@ -1,12 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
-public abstract class User {
-private String name;
-private String password;
-private String username;
-private role RoleType;
-private int employeeID;
-	public boolean login(String name,String password) {
-		
-	}
-	 
+/**
+ *
+ * @author vennela
+ */
+
+    
+public class User {
+    
+    private String username;
+    private String password;
+    private String name;
+    private Role roletype;
+    private int employeeID;
+    
+    //Methods
+    
+    public boolean login (String username, String password){
+        return false;
+        
+    
+}
+
+public String getUsername(){
+        return null;
+    
+}
+
+public String getPassword(){
+        return null;
+    
+}
+
+public String getName(){
+        return null;
+    
+}
+
+
+public Role getRoleType(){
+        return null;
+       // return null;
+    
+}
+
+
+public int getEmployeeID(){
+        return 0;
+
+}
+
+public void setUsername( String username){
+    this.username=username;
+}
+
+public void setPassword( String password){
+    this.password=password;
+}
+
+public void setName( String name){
+    this.name=name;
+}
+
+public void setRoleType( Role roletype){
+    this.roletype=roletype;
+}
+public void employeeID( int employeeID){
+    this.employeeID=employeeID;
+}
 }

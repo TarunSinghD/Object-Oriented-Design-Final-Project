7d9eaec571c525cb8e11e1a02aae9af897086105
Vennela Gandluri <vennela@Mahadevs-MacBook-Pro.local>
2018-03-20 23:48:42 -0600

    All classes

---
+++ LeaveManagement/src/LeaveApplication.java
@@ -0,0 +1,56 @@
+
+public class LeaveApplication {
+	
+	private int employeeID;
+    private Leave leaveType;
+	private int noOfDays;
+	private int applicationID ;
+	private Status applicationStatus;
+	
+	public LeaveApplication createLeaveApp (int employeeID ,Leave leaveType , int noOfDays ) {
+		return null;
+
+	}
+	public void viewLeaveApp() {
+		
+	}
+	public void  updateLeaveAppStatus(Status applicationStatus) {
+		
+	}
+	public Leave getLeaveType() {
+		return leaveType;
+		
+	}
+	public int getEmployeeID() {
+		return 0;
+		
+	}
+	public int getNoOfDays() {
+		return 0;
+		
+	}
+	public int getApplicationID() {
+		return applicationID;
+		
+	}
+	public Status getApplicationStatus() {
+		return applicationStatus;
+		
+	}
+	public void setLeaveType(Leave leaveType ) {
+		
+	}
+	public void setEmployeeID(int employeeID ) {
+		
+	}
+	public void setNoOfDays(int noOfDays ) {
+		
+	}
+	public void setApplicationID(int applicationID ) {
+		
+	}
+	public void  setApplicationStatus(Status applicationStatus ) {
+		
+	}
+
+}

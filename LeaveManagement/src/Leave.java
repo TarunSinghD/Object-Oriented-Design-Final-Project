42e5cc1130a8994a4542e38ae65691e68fed2413
Rohithsali <rosa8914@colorado.edu>
2018-03-20 22:16:05 -0600

    added role, Leave, Manager, Status

---
+++ LeaveManagement/src/Leave.java
@@ -0,0 +1,6 @@
+
+public enum Leave {
+	Sick,
+	Casual,
+	Paid
+}
---
+++ LeaveManagement/src/Manager.java
@@ -0,0 +1,27 @@
+
+public class Manager extends User {
+	private ArrayList<LeaveApplication> empLeaveRequestQ;
+	private int managerID;
+	private ArrayList<employee> empList;
+	public ArrayList<LeaveApplication> viewLeaveRequest() {
+		
+	}
+	
+	public void approveLeaveRequest() {
+		
+	}
+	
+	public void denyLeaveRequest() {
+		
+	}
+	
+	public int getManagerID() {
+		
+		return managerID;
+	}
+	
+	public void setManagerID(int managerID) {
+		
+	}
+
+}

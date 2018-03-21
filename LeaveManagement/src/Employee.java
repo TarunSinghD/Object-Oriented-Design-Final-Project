fec44970e17530d9ad2000d73ab95fc86009bce2
Rohithsali <rosa8914@colorado.edu>
2018-03-20 21:30:35 -0600

    inital work

---
+++ LeaveManagement/src/Employee.java
@@ -0,0 +1,48 @@
+
+public class Employee extends User {
+	private ArrayList<LeaveApplication> empLeaveRequestQ;
+	private int managerID;
+	private LeaveBalance empLeaveBalance;
+	private Leave leaveType;
+	
+	public void applyLeave() {
+		
+	}
+	
+	public void checkLeaveBalance(Leave leaveType) {
+		
+	}
+	
+	public void viewApplicationStatus() {
+		
+	}
+	
+	public void cancelLeave() {
+		
+	}
+	public void updateLeaveRequest() {
+		
+	}
+	
+	public int getManagerID() {
+		
+		return managerID;
+	}
+	
+	public int getLeaveBalance() {
+		
+	}
+	
+	public Leave getLeaveType() {
+		
+	}
+	
+	public void setManagerID(int managerID) {
+		
+	}
+	
+	public void setLeaveType(Leave leaveType) {
+		
+	}
+	
+}
---
+++ LeaveManagement/src/User.java
@@ -0,0 +1,12 @@
+
+public abstract class User {
+private String name;
+private String password;
+private String username;
+private role RoleType;
+private int employeeID;
+	public boolean login(String name,String password) {
+		
+	}
+	 
+}
